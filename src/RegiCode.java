ef6df4b64bb0ae85ec2868345824c77f9bdd2423
paulamanz <pamaiz@gmail.com>
2019-10-02 16:33:49 +0200

    Fixed UI

--- src/RegiCode.java
+++ src/RegiCode.java
@@ -10,6 +10,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,10 +21,13 @@ import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 import javax.swing.TransferHandler;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -35,20 +39,24 @@ public class RegiCode extends JFrame {
 	private JPanel mainPanel, methodPanel;
 	private JTextField mainTextField, starTextField;
 	private JTable table;
-	private ImageIcon ball = new ImageIcon("src/ball.jpg");
+	private ImageIcon regi = new ImageIcon("src/ghost.png");
+	private ImageIcon planet = new ImageIcon("src/saturn.png");
+	private ImageIcon meteorite = new ImageIcon("src/meteorite.png");
 
-	private String [] colunas = {"1", "2", "3", "4"};
+	private String [] colunas = {"1", "2", "3", "4", "5", "6", "7"};
 	private Object [][] dados = {
-		    {ball, "", "", ""},
-		    {"", "", "", ""},
-		    {"", "", "", ""},
-		    {"", "", "", ""}
+		    {regi, "", "", "", "", "", meteorite},
+		    {"", "", "", "", "", "", ""},
+		    {"", "", meteorite, "", "", "", ""},
+		    {"", "", "", "", "", "", ""},
+		    {"", meteorite, "", "", "", "", ""},
+		    {"", "", "", "", "", "", planet}
 		};
 
 	public RegiCode() {
 		super("RegiCode");
-		this.setPreferredSize(new Dimension(800,800));
-		this.setMinimumSize(new Dimension(500, 500));
+		this.setPreferredSize(new Dimension(1800,900));
+		this.setMinimumSize(new Dimension(900, 450));
 
 		setUpUI();
 		this.pack();
@@ -74,20 +82,22 @@ public class RegiCode extends JFrame {
 
 		};
 
-
-
 		mainPanel = new JPanel();
 		mainPanel.setBackground(Color.BLACK);
 		table = new JTable(model);
-		table.setRowHeight(65);
+		
+		TableColumnModel columnModel = table.getColumnModel();
+		columnModel.getColumn(0).setPreferredWidth(100);
+		columnModel.getColumn(1).setPreferredWidth(100);
+		columnModel.getColumn(2).setPreferredWidth(100);
+		columnModel.getColumn(3).setPreferredWidth(100);
+		columnModel.getColumn(4).setPreferredWidth(100);
+		columnModel.getColumn(5).setPreferredWidth(100);
+		columnModel.getColumn(6).setPreferredWidth(100);
+		
+		table.setRowHeight(100);
 		table.setRowSelectionAllowed(false);;
 		mainPanel.add(table);
-		//ImageIcon loop2icon = new ImageIcon("src/ball.jpg");
-		//loop2 = new JButton(loop2icon);
-		//mainPanel.add(loop2);
-
-		//table.setValueAt(loop2icon, 0, 0);
-
 
 		getContentPane().add(mainPanel, BorderLayout.CENTER);
 	}
@@ -98,59 +108,59 @@ public class RegiCode extends JFrame {
 		GridBagConstraints c = new GridBagConstraints();
 		c.weightx = 10;
 		c.weighty = 10;
-		c.gridwidth = 2;
 		c.insets = new Insets(5,5,5,5);
 		c.fill = GridBagConstraints.HORIZONTAL;
 
-
-		//methodPanel.setLayout(new GridLayout(0,1));
 		methodPanel.setBackground(new Color(247,247,247));
 
 
 
-		JLabel labelMain = new JLabel("Main Method:");
-		labelMain.setFont(new Font("Helvetica Neue", Font.PLAIN, 20));
+		JLabel labelMain = new JLabel("Main");
+		labelMain.setFont(new Font("Rockwell", Font.PLAIN, 20));
 		c.gridx = 0;
 		c.gridy = 0;
-		c.gridheight = 5;
 		labelMain.setVerticalTextPosition(JLabel.BOTTOM);
 		methodPanel.add(labelMain, c);
 
 		mainTextField = new JTextField(30);
 		mainTextField.setDragEnabled(true);
-		c.gridx= 0;
-		c.gridy = 1;
-		c.ipady = 100;
+		c.gridwidth = 3;
+		c.gridx= 1;
+		c.gridy = 0;
+		c.ipady = 150;
 		methodPanel.add(mainTextField,c);
 
 
-
-		JLabel labelStar = new JLabel("Star Method:");
-		labelStar.setFont(new Font("Helvetica Neue", Font.PLAIN, 20));
+		Icon staricon = new ImageIcon("src/star.png");
+		JLabel labelStar = new JLabel(staricon);
+		//labelStar.setFont(new Font("Rockwell", Font.PLAIN, 20));
+		c.gridwidth = 1;
 		c.gridx=0;
-		c.gridy=2;
+		c.gridy=1;
 		c.ipady = 0;
 		methodPanel.add(labelStar, c);
 
 
 		starTextField = new JTextField(30);
+		c.gridwidth = 3;
 		c.ipady = 100;
-		c.gridx= 0;
-		c.gridy = 3;
+		c.gridx= 1;
+		c.gridy = 1;
 		methodPanel.add(starTextField,c);
 
 		Icon playicon = new ImageIcon("src/fast-forward.png");
 		play = new JButton(playicon);
-		c.gridwidth = 1;   //2 columns wide
+		c.gridwidth = 2;   //2 columns wide
 		c.ipady = 0;
 		c.gridx=1;
-		c.gridy=4;
+		c.gridy=2;
 		methodPanel.add(play, c);
 
 		Icon stepicon = new ImageIcon("src/play.png");
 		playstep = new JButton (stepicon);
+		//c.gridwidth = 2;
 		c.gridx=0;
-		c.gridy = 4;
+		c.gridy = 2;
 		methodPanel.add(playstep, c);
 
 
@@ -159,42 +169,51 @@ public class RegiCode extends JFrame {
 
 
 	public void setUpToolBar() {
+		
+//		MouseAdapter listener = new MouseAdapter() {
+//			
+//		}
 
 		toolBar = new JToolBar("My ToolBar");
 		toolBarGroup = new ButtonGroup();
 
-		Icon loop2icon = new ImageIcon("src/repeat.png");
+		Icon loop2icon = new ImageIcon("src/2times.png");
 		loop2 = new JButton(loop2icon);
 		loop2.setTransferHandler(new TransferHandler("Loop twice"));
 		toolBarGroup.add(loop2);
 		toolBar.add(loop2);
 
-		Icon loop3icon = new ImageIcon("src/repeat.png");
+		Icon loop3icon = new ImageIcon("src/3times.png");
 		loop3 = new JButton (loop3icon);
 		toolBarGroup.add(loop3);
 		toolBar.add(loop3);
 
-		Icon loop4icon = new ImageIcon("src/repeat.png");
+		Icon loop4icon = new ImageIcon("src/4times.png");
 		loop4 = new JButton(loop4icon);
 		toolBarGroup.add(loop4);
 		toolBar.add(loop4);
+//		loop4.addMouseListener(listener);
+//	    loop4.addMouseMotionListener(listener);
 
 		toolBar.addSeparator(new Dimension(100,30));
 
-		Icon lefticon = new ImageIcon("src/reply.png");
+		Icon lefticon = new ImageIcon("src/left.png");
 		left = new JButton(lefticon);
 		toolBarGroup.add(left);
 		toolBar.add(left);
+		Icon righticon = new ImageIcon("src/right.png");
+		right = new JButton(righticon);
+		toolBarGroup.add(right);
+		toolBar.add(right);
+		
+		toolBar.addSeparator(new Dimension(100,30));
 
-		Icon straighticon = new ImageIcon("src/arrow-up.png");
+		Icon straighticon = new ImageIcon("src/straight.png");
 		straight = new JButton(straighticon);
 		toolBarGroup.add(straight);
 		toolBar.add(straight);
 
-		Icon righticon = new ImageIcon("src/share.png");
-		right = new JButton(righticon);
-		toolBarGroup.add(right);
-		toolBar.add(right);
+		
 
 		toolBar.addSeparator(new Dimension(100,30));
 
@@ -205,5 +224,27 @@ public class RegiCode extends JFrame {
 
 		this.getContentPane().add(toolBar, BorderLayout.NORTH);
 	}
+	
+	public void makeUI() {
+		 
+	    MouseAdapter listener = new MouseAdapter() {
+	 
+	      Point p = null;
+	 
+	      @Override
+	      public void mousePressed(MouseEvent e) {
+	        p = e.getLocationOnScreen();
+	      }
+	 
+	      @Override
+	      public void mouseDragged(MouseEvent e) {
+	        JComponent c = (JComponent) e.getSource();
+	        Point l = c.getLocation();
+	        Point here = e.getLocationOnScreen();
+	        c.setLocation(l.x + here.x - p.x, l.y + here.y - p.y);
+	        p = here;
+	      }
+	    };
+	  }
 
 }
